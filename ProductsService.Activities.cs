413086e705fb4c978becc17572ceb767959750d3
Furkan Özbay <furkanozb37@hotmail.com>
2021-03-14 03:36:38 +0300

    Add project files.

---
+++ ProductsService.Activities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using System.ServiceModel;
+using _ProductsModel;
+
+namespace _ProductsWorkflowService
+{
+    
+    public sealed class ProductExists : CodeActivity<Boolean>
+    {
+        public InArgument<NorthwindEntities> Database { get; set; }
+        public InArgument<int> ProductId { get; set; }
+
+
+        protected override bool Execute(CodeActivityContext context)
+        {
+            int productId = ProductId.Get(context);
+            NorthwindEntities database = Database.Get(context);
+            int numProducts = (from p in database.Products
+                               where p.ProductID == productId
+                               select p).Count();
+            return numProducts > 0;
+        }
+
+    }
+
+    public sealed class FindProduct : CodeActivity<Product>
+    {
+        public InArgument<NorthwindEntities> Database { get; set; }
+        public InArgument<int> ProductId { get; set; }
+
+        protected override Product Execute(CodeActivityContext context)
+        {
+            int productId = ProductId.Get(context);
+            NorthwindEntities database = Database.Get(context);
+            Product matchingProduct = database.Products.First(p=>p.ProductID == productId);
+            return matchingProduct;
+        }
+    }
+}
